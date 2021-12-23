a55a0f697d66ccf760d56df7def5cfb85e87dac9
Riya <9038riya@gmail.com>
2021-12-23 23:49:53 +0530

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ButtonTab from "./ButtonTab";
 import OutputScreen from "./OutputScreen ";
+var operand1=0;
+var operand2=null;
+var operator=null;
 
 class App extends React.Component {
   //adding states
@@ -12,46 +15,70 @@ class App extends React.Component {
     }
 
     this.handleClickButton= this.handleClickButton.bind(this);
+    this.isOperator= this.isOperator.bind(this);
   }
 
+  isOperator(value){
+    return value === "+" || value === "-" || value === "*" || value === "/";
+  }
   //handles all onclick events
   handleClickButton(event){
     const value= event.target.value;
-    switch(value){
+    var text = Number(this.state.display);
+    
+    if(this.isOperator(value))
+		{
+			operand1=Number(text);
+			operator=value;
+      var str = this.state.display;
+      str = str.substring(str.length-1);
+      if(str!=='+' && str!=='-' && str!== '*' && str!=='/')
+			  this.setState({display: this.state.display + operator});
+      else{
+        this.setState({display: this.state.display + ''});
+      }
 
-      case '=':{
-        if(this.state.display!==""){
-          var ans="";
-          try{
-            ans= eval(this.state.display);
-          }
-          catch(err){
-            this.setState({display:"Math Error"});
-          }
-          if(ans=== undefined){
-            this.setState({display:"Math Error"});
-          }
-          else{
-            this.setState({display: ans});
-          }
-        }
-        break;
-      }
-      case 'Clear':{
-        this.setState({display:""});
-        break;
-      }
-      case 'Delete': {
-        var s = this.state.display;
-        s = s.substring(0,s.length-1);
-        this.setState({display: s});
-        break;
-      }
-      default:{
-        this.setState({ display: this.state.display += value})
-        break;
-      }
-    }
+		}
+		else if(value==="Clear")
+		{
+			this.setState({display:""});
+		}
+		else if(value===".")
+		{
+			if(this.state.display.length && !this.state.display.includes('.'))
+			{
+				this.setState({display: this.state.display +'.'});
+			}
+		}
+		else if(value==="%")
+		{
+			operand1=Number(text);
+			operand1=operand1/100;
+			this.setState({display:operand1}); 
+		}
+		else if(value==="Delete")
+		{
+			var s = this.state.display;
+      s = s.substring(0,s.length-1);
+      this.setState({display: s});
+
+		}
+		else if(value==="=")
+		{
+			operand2=Number(text);
+			var result=eval(this.state.display);
+      result= String(result);
+			
+      this.setState({display:result});
+      operand1=0;
+      operand2=null;
+      operator=null;
+		}
+		else
+		{
+			this.setState({ display: this.state.display += value})
+		}
+    
   }
 //main app component that has display screen and buttons
   render() {
